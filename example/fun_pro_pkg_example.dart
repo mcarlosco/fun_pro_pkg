f81b6a798113291f6e1544b48ab989cfa4bc5ba5
Carlos Murillo <m.carlos.co@gmail.com>
2024-11-12 18:01:22 -0500

    update fun_pro_pkg_example.dart

--- example/fun_pro_pkg_example.dart
+++ example/fun_pro_pkg_example.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'package:fun_pro_pkg/fun_pro_pkg.dart';
+
+final class InfinityQuotientException implements Exception {}
+
+Coin<double, Exception> divide(int dividend, int divisor) => divisor != 0
+    ? Coin.heads(dividend / divisor)
+    : Coin.tails(InfinityQuotientException());
+
+void main() {
+  divide(5, 2).flip<void>(print, print);
+  divide(5, 0).flip<void>(print, print);
+}
